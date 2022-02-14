97524e521be84ae3a0d90fdc30cad33558454e3c
苏飞 <fei.su@imcore.com>
2022-02-14 17:07:07 +0800

    android client

--- Assets/GameMain/Scripts/Entity/EntityLogic/Bird.cs
+++ Assets/GameMain/Scripts/Entity/EntityLogic/Bird.cs
@@ -26,6 +26,7 @@ namespace FlappyBird
         private float m_ShootTimer = 0f;
 
         private Rigidbody2D m_Rigidbody = null;
+        private float ScreenWidth;
 
         protected override void OnShow(object userData)
         {
@@ -39,6 +40,7 @@ namespace FlappyBird
             }
             //重置射击冷却
             m_ShootTimer = 10f;
+            ScreenWidth = Screen.width;
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
@@ -47,7 +49,7 @@ namespace FlappyBird
 
 
             //上升控制
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) || IsTouchLeft())
             {
                 GameEntry.Sound.PlaySound(2);
                 m_Rigidbody.velocity = new Vector2(0, m_BirdData.FlyForce);
@@ -55,12 +57,82 @@ namespace FlappyBird
 
             //射击控制
             m_ShootTimer += elapseSeconds;
-            if (m_ShootTimer >= m_ShootTime && Input.GetKeyDown(KeyCode.J))
+            if (m_ShootTimer >= m_ShootTime && (Input.GetKeyDown(KeyCode.J) || IsTouchRight()))
             {
                 m_ShootTimer = 0;
                 GameEntry.Sound.PlaySound(3);
                 GameEntry.Entity.ShowBullet(new BulletData(GameEntry.Entity.GenerateSerialId(), 4, CachedTransform.position + CachedTransform.right, 6));
             }
+        }
+
+        private bool IsTouchRight()
+        {
+            if (Input.touchCount == 1)
+            {
+                Touch t1 = Input.GetTouch(0);
+                if (t1.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x < ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (Input.touchCount == 2)
+            {
+                Touch t1 = Input.GetTouch(0);
+                Touch t2 = Input.GetTouch(1);
+                if (t1.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x < ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+                else if (t2.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x < ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IsTouchLeft()
+        {
+            if (Input.touchCount == 1)
+            {
+                Touch t1 = Input.GetTouch(0);
+                if (t1.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x > ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (Input.touchCount == 2)
+            {
+                Touch t1 = Input.GetTouch(0);
+                Touch t2 = Input.GetTouch(1);
+                if (t1.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x > ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+                else if (t2.phase == TouchPhase.Ended)
+                {
+                    if (t1.position.x > ScreenWidth / 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
